bbe8e018195aed165e5fbebca1125b9c238eec0d
Ryan Schanzenbacher <ryan@rschanz.org>
2022-07-21 08:24:43 -0400

    Added inital scaffolding

---
+++ backend_srv/__init__.py
@@ -0,0 +1,10 @@
+######################
+
+Smishing Server entrypoint
+
+######################
+
+# TODO:
+#   - Setup twilio account and information
+#   - Spawn the HTTP listener (if using python for backend <-> frontend communication
+#   - Setup helper functions to allow SMS sending using twilio api
