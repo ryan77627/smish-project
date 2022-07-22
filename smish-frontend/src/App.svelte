3b8ee34bef862f25a248e376dd33eab9aaa66f4d
Ryan Schanzenbacher <ryan@rschanz.org>
2022-07-21 22:14:45 -0400

    Added scaffolding for frontend maybe

---
+++ smish-frontend/src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+</script>
+
+<main>
+    <div class="login-form">
+        <h1>Login</h1>
+        <form id="login">
+            <label>Username</label>
+            <input type="text" />
+            <label>Password</label>
+            <input type="password" />
+        </form>
+    </div>
+</main>
+
+<style>
+</style>
