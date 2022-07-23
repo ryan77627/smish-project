48f36a5e70c8edbed7fd05db6506413ad728b8a6
AOrps <aeo34@njit.edu>
2022-07-22 20:32:47 -0400

    CODEOWNERS + twilio send functionality + .gitignore

--- twilio/main.py
+++ twilio/main.py
@@ -1,2 +1,14 @@
 #!/usr/bin/env python3
+from creds import T_SID, T_TOKEN, SenderNumber
+from twilio.rest import Client
 
+# send_sms: sms message
+def send_sms(client, body: str, sender: str, receiver: str):
+    client.messages.create(body=body,from_=sender,to=receiver)
+
+def receive_sms():
+    pass
+
+if __name__ == "__main__":
+    client = Client(T_SID, T_TOKEN)
+    print(type(client))
