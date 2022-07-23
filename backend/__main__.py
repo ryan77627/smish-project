8a21d246ca07bb877c0b4165e08eaac8bf7ed72c
Ryan Schanzenbacher <ryan@rschanz.org>
2022-07-22 21:48:22 -0400

    successfully added route!

--- backend/__main__.py
+++ backend/__main__.py
@@ -6,7 +6,6 @@
 from sanic import Sanic
 import backend.submitSmish as submitSmish
 
-
 def main():
 
     app = Sanic("endpoint-listener")
@@ -14,6 +13,10 @@ def main():
     @app.route('/submit-smish')
     async def handler(request):
         return submitSmish.submit(request) # There may be better ways to do this, may refactor later on with actual classes?
+
+    
+    # We are ready, deploy!
+    app.run(host='0.0.0.0', port=1234, access_log=False, workers=2)
 
 if __name__ == "__main__":
     main()
