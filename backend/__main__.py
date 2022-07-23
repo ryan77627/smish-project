63d652a32b93e40303fd990a0f600534ffa1b03d
Ryan Schanzenbacher <ryan@rschanz.org>
2022-07-22 21:36:17 -0400

    renamed entrypoint file and added scaffolding

---
+++ backend/__main__.py
@@ -0,0 +1,19 @@
+######################
+
+# Smishing Server entrypoint
+
+######################
+from sanic import Sanic
+import backend.submitSmish as submitSmish
+
+
+def main():
+
+    app = Sanic("endpoint-listener")
+
+    @app.route('/submit-smish')
+    async def handler(request):
+        return submitSmish(request) # There may be better ways to do this, may refactor later on with actual classes?
+
+if __name__ == "__main__":
+    main()
