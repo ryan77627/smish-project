c2043e89b79bd5745202145cacf5587415ef23e3
Ryan Schanzenbacher <ryan@rschanz.org>
2022-07-25 22:18:35 -0400

    url basic parser works

--- backend/endpoints.py
+++ backend/endpoints.py
@@ -4,12 +4,14 @@
 import pickle
 import random
 import string
+import io
 
 def gen_url():
+    campaign_id = 'test-id'
     phish_db = open_db(campaign_id) # Returns an empty/populated dictionary
     # This will eventually take a user as a parameter
     uid = 1
-    rand_string = random.choices(string.ascii_letters, k=5)
+    rand_string = ''.join(random.choices(string.ascii_letters, k=5))
     phish_db[rand_string] = uid
 
     ret_url = '/login-' + rand_string
@@ -21,24 +23,26 @@ def gen_url():
 
 def open_db(phish_campaign):
     # Unpickle and open the database for writing
-    with open(f'phish_db/{phish_campaign}.db', 'w') as f:
+    with open(f'phish_db/{phish_campaign}.db', 'rb+') as f:
         try:
             phish_db = pickle.load(f)
-        except io.UnsupportedOperation:
+        except EOFError as e:
+            print(e)
             # File doesn't exist, create a new campaign db and track it
             phish_db = dict()
 
-    with open('phish_campaigns.db', 'w') as e:
+    with open('phish_campaigns.db', 'rb+') as e:
         try:
             db = pickle.load(e)
-        except io.UnsupportedOperation:
+        except EOFError:
             db = []
-        db.append(phish_campaign)
-        db = pickle.dump(db, e)
+        if phish_campaign not in db:
+            db.append(phish_campaign)
+            db = pickle.dump(db, e)
 
 
     return phish_db
 
-def close_db(phish_campaign, phish_db):
-    with open(f'phish_db/{phish_campaign}.db', 'w') as f:
+def save_db(phish_campaign, phish_db):
+    with open(f'phish_db/{phish_campaign}.db', 'wb+') as f:
         pickle.dump(phish_db, f)
