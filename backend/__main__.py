93885e4cae70b7c1b6407bd57657e5511be7454c
Ryan Schanzenbacher <ryan@rschanz.org>
2022-07-26 22:24:03 -0400

    added another route to grab user tree publically

--- backend/__main__.py
+++ backend/__main__.py
@@ -8,19 +8,26 @@ sys.path.append('../')
 from sanic import Sanic
 import backend.submitSmish as submitSmish
 import backend.userStore as userStore
+import backend.api as api
+
+# Initialize any users in the DB
+us = userStore.UserStore()
 
 def main():
 
     app = Sanic("endpoint-listener")
 
-    # Initialize any users in the DB
-    userStore = userStore.UserStore()
 
     # Submit phish route
     app.add_route(submitSmish.View.as_view(), '/submit-smish')
+
+    # Submit frontend gen user tree route
+    app.add_route(api.genTree.as_view(), '/api/genTree')
     
     # We are ready, deploy!
     app.run(host='0.0.0.0', port=1234, access_log=False, workers=2)
+
+    us = None
 
 if __name__ == "__main__":
     main()
