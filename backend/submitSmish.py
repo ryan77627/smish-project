f4fe849f62f3ab6faf6aa131abea445b1912f08b
Ryan Schanzenbacher <ryan@rschanz.org>
2022-07-28 15:00:55 -0400

    added user lookup for destination phone number

--- backend/submitSmish.py
+++ backend/submitSmish.py
@@ -2,6 +2,7 @@ from sanic import Sanic, text
 from sanic.views import HTTPMethodView
 from backend.twilio import send as twilio
 from backend import endpoints
+import backend.__main__ as config
 import json
 # Module that is called when '/submit-smish' is POSTed to
 
@@ -17,7 +18,7 @@ class View(HTTPMethodView):
         # Parse the message to be the message with the URL
         msg = req['msg'].format(URL = BASE_DOMAIN + endpoints.gen_url(req['uid']))
         try:
-            twilio.Send(req["to"], msg)
+            twilio.Send(us.get_user(req['uid']).phonenum, msg)
             return text("Successful!")
             #return text(msg)
         except Exception as e:
